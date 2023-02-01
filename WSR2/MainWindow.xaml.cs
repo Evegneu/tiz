3e1bff3eb909d25fa1750787a4a787f652ad948f
Evegneu <evgeneu1998@mail.ru>
2023-02-01 11:36:21 +0300

    test commit

---
+++ WSR2/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace WSR2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var auth = Class1.db.User.Where(p => p.UserLogin == loginBox.Text && p.UserPassword == passwordBox.Text).FirstOrDefault();
+        if (auth !=null)
+            {
+                Window1 Frame2 = new Window1();
+                this.Hide();
+                Frame2.Show();
+            }
+            else
+            {
+                MessageBox.Show("Проверьте логин или пароль");
+            }
+        }
+    }
+}
---
+++ WSR2/Window1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WSR2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            MyListBox.ItemsSource = Class1.db.Product.ToList();
+            Combo2.SelectedIndex = 0;
+        }
+    }
+}
